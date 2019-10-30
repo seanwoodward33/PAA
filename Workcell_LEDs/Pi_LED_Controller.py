809715355b0e586fbd65df26181a58e2c1d76c66
seanwoodward33 <35349749+seanwoodward33@users.noreply.github.com>
2019-10-30 20:42:13 +0000

    Update Pi_LED_Controller.py

--- Workcell_LEDs/Pi_LED_Controller.py
+++ Workcell_LEDs/Pi_LED_Controller.py
@@ -45,6 +45,7 @@ def TheatreChase(strip, colour, waitTime=50, iterations=10):    #waitTime is in 
     for i in range(iterations):
         for j in range(3):
             for k in range(0,len(strip)-1,3):
+                print(k+j)
                 strip[k+j] = colour
             strip.show()
             time.sleep(waitTime/1000.0)
