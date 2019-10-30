a42d2a22698bd03626d752ba5e2f037d9b5f097c
seanwoodward33 <35349749+seanwoodward33@users.noreply.github.com>
2019-10-30 20:43:44 +0000

    Update Pi_LED_Controller.py

--- Workcell_LEDs/Pi_LED_Controller.py
+++ Workcell_LEDs/Pi_LED_Controller.py
@@ -19,7 +19,7 @@ import threading
 #Define LED strip configuration
 def LedSetup(ledPin = board.D18, ledCount = 25, ledOrder = neopixel.GRB):
     ledPin = board.D18						                        #GPIO pin LEDs are connected to Pi
-    ledCount = 25							                        #Number of LEDs in strip
+    ledCount = 1							                        #Number of LEDs in strip
     ledOrder = neopixel.GRB                                         #Set to *.GRB or *.RGB depending on how LEDs are wired
     ledStrip = neopixel.NeoPixel(ledPin, ledCount, brightness=0.2, auto_write=False, pixel_order=ledOrder)
     return ledStrip
