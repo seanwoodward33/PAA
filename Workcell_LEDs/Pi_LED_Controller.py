33a19a2355f7d24fcd6d429db32f114eaf7868ff
Sean Woodward <Sean_Woodward@Falcon.local>
2019-11-01 10:30:05 +0000

    Add PAA Colours definition

--- Workcell_LEDs/Pi_LED_Controller.py
+++ Workcell_LEDs/Pi_LED_Controller.py
@@ -21,7 +21,7 @@ def LedSetup(ledPin = board.D18, ledCount = 25, ledOrder = neopixel.GRB):
     ledPin = board.D18						                        #GPIO pin LEDs are connected to Pi
     ledCount = 98							                        #Number of LEDs in strip
     ledOrder = neopixel.GRB                                         #Set to *.GRB or *.RGB depending on how LEDs are wired
-    ledStrip = neopixel.NeoPixel(ledPin, ledCount, brightness=1.0, auto_write=False, pixel_order=ledOrder)
+    ledStrip = neopixel.NeoPixel(ledPin, ledCount, brightness=0.5, auto_write=False, pixel_order=ledOrder)
     return ledStrip
 
 #Define functions to control LEDs
@@ -103,39 +103,44 @@ def RunState(strip):
     strip.fill((0,255,0))
     strip.show()
 
+#Set solid colour
+def SolidColour (strip, colour):
+    strip.fill(colour)
+    strip.show()
+    
+#Colour definitions
+paaDarkBlue = (2,116,153)
+paaLightBlue = (53,177,203)
+paaSlabPri = (201,208,34)
+paaSlabSec = (77,138,45)
+paaScelPri = (230,81,151)
+paaScelSec = (151,19,75)
+paaKx2Pri = (247,175,00)
+paaKx2Sec = (239,121,38)
+paaSrunPri = (81,40,125)
+paaSrunSec = (60,23,92)
+paaStorPri = (00,144,208)
+paaStorSec = (00, 89, 163)
+
+
 #Main program logic
 if __name__ == '__main__':
     #Initialise LED strip
     ledStrip = LedSetup()
+
+
     
     #Testing Loop
     try:
         while True:
-            #ColourWipe(ledStrip, (255,0,0), 0)
-            #ColourWipe(ledStrip, (0,255,0), 0)
-            #ColourWipe(ledStrip, (0,0,255), 0)
-            #ColourWipeTwo(ledStrip, (0,255,0))
-            #TheatreChase(ledStrip, (255,255,255))
-            #SinglePixelWipe(ledStrip,(255,0,255))
+            ColourWipe(ledStrip, (255,0,0), 0)
+            ColourWipe(ledStrip, (0,255,0), 0)
+            ColourWipe(ledStrip, (0,0,255), 0)
+            ColourWipeTwo(ledStrip, (0,255,0))
+            TheatreChase(ledStrip, (255,255,255))
+            SinglePixelWipe(ledStrip,(255,0,255))
             Rainbow(ledStrip)
-            #SinglePixelWipeRetain(ledStrip,(255,0,255))
-            #ErrorState(ledStrip)
-            #time.sleep(1)
-            #RunState(ledStrip)
-            #time.sleep(1)
-            ledStrip.fill((2,116,153))
-            ledStrip.show()
-            time.sleep(5)
-            ledStrip.fill((53,177,203))
-            ledStrip.show()
-            time.sleep(5)
-            ledStrip.fill((230,81,151))
-            ledStrip.show()
-            time.sleep(5)
-            ledStrip.fill((151,19,75))
-            ledStrip.show()
-            time.sleep(5)
-            #Rainbow(ledStrip,10)
+            SinglePixelWipeRetain(ledStrip,(255,0,255))
             
     except KeyboardInterrupt:
         ColourWipe(ledStrip, (0,0,0), 10)
