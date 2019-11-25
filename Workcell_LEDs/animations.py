076ee96fcd708db64009ee01112f679b620f731c
Sean Woodward <Sean_Woodward@Falcon.local>
2019-11-25 11:46:45 +0000

    Attempt to remove support for multiple strips of LEDs

--- Workcell_LEDs/animations.py
+++ Workcell_LEDs/animations.py
@@ -148,7 +148,6 @@ def Rainbow(strip, waitTime=10, q=q):
                 strip[j] = HsvToRgb((((j+i)%ledCount)/ledCount),1.0,1.0)
             strip.show()
             logging.debug("queue is: " + str(q.qsize()))
-            time.sleep(1)
 
 #Knightrider
 def Knightrider(strip, waitTime, q=q):
