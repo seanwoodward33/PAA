c448280b67b391c4c2868aaf34a331e05f0b859f
Sean Woodward <Sean_Woodward@Falcon.local>
2019-11-25 11:27:48 +0000

    Attempt to remove support for multiple strips of LEDs

--- Workcell_LEDs/animations.py
+++ Workcell_LEDs/animations.py
@@ -22,7 +22,7 @@ q=queue.Queue()
 
 #Function to check wait loop
 def ThreadCheck(q, runLoop):
-    logging.debug("checking queue")
+    #logging.debug("checking queue")
     if q.empty == False:
         logging.debug("exiting animation")
         x = q.get()
