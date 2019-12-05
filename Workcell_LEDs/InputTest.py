79145e2ca5f215c127047835007b221cfa9a8ea2
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-05 12:23:48 +0000

    Add testing script for input

--- Workcell_LEDs/InputTest.py
+++ Workcell_LEDs/InputTest.py
@@ -3,16 +3,20 @@
 Created on Thu Dec  5 12:01:17 2019
 
 @author: Sean_Woodward
-"""
 
+Script to test GPIO inputs
+"""
+#Import relavent libraries
 import board
 import digitalio
 import time
 
+#Setup input
 button = digitalio.DigitalInOut(board.D4)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
+#run while loop to test
 while True:
     if button.value == 1:
         print ("high value received")
