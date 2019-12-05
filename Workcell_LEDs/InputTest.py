f02e5ce5f6515e853b95683eba6eca24e8444f84
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-05 12:21:38 +0000

    Add testing script for input

--- Workcell_LEDs/InputTest.py
+++ Workcell_LEDs/InputTest.py
@@ -11,7 +11,7 @@ import time
 
 button = digitalio.DigitalInOut(board.D4)
 button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.DOWN
+button.pull = digitalio.Pull.UP
 
 while True:
     if button.value == 1:
