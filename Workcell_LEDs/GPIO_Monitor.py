64067a6ea7dbf497ffb13507c0632098b9555c12
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-05 18:31:20 +0000

    Add SLAS main subfolder

---
+++ Workcell_LEDs/GPIO_Monitor.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 13:30:25 2019
+
+@author: Sean_Woodward
+"""
+
---
+++ Workcell_LEDs/SLAS_Controller/SlasMain.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 13:39:07 2019
+
+@author: Sean_Woodward
+
+Main.py - Program to run to control whole system
+"""
+
+#Import libraries
+import threading
+import queue
+import time
+import logging
+import board
+import neopixel
+
+#Setup logging
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] - %(asctime)s - (%(threadName)-10s) %(message)s')
+
+#Define workcell class
+class Workcell():
+    def __init__(self):
+        pass
+    
+    def LedSetup(self, ledGpioPin, ledCount, ledBrightness, ledOrder = neopixel.GRB):
+        self.ledPin = ledGpioPin
+        self.ledCount = ledCount
+        self.ledBrightness = ledBrightness
+        self.ledOrder = ledOrder
+    
+    def LedInitialise(self):
+        self.ledStrip = neopixel.NeoPixel(self.ledPin, self.ledCount, brightness = self.ledBrightness, pixel_order=self.ledOrder, auto_write=False)
+    
+    def LedSections(self, sections = [[1,[0,0]]]):
+        self.ledSections = sections
+    
+    def PrintLedSections(self):
+        print (self.LedSections)
+        
+
+
+
+
+if __name__ == '__main__':
+    logging.debug("Main SLAS control program running")
+    
+    logging.debug("Create SLAS workcell object")
+    SLAS = Workcell()
+    
+    logging.debug("Create SLAS Workcell object")
+    SLAS.LedSetup(18, 98, 0.2)
+    
+    logging.debug("Initialise LEDs")
+    SLAS.LedInitialise()
+    
+    logging.debug("Setting up LED sections")
+    SLAS([[1,[0,98]]])
+    
+    logging.debug("Printing LED sections")
+    SLAS.PrintLedSections()
