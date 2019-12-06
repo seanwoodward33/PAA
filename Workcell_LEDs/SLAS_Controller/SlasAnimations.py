664464f702097812d68b8da55892458d1760953a
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-06 17:53:34 +0000

    Add SLAS main subfolder

---
+++ Workcell_LEDs/SLAS_Controller/SlasAnimations.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 10:06:26 2019
+
+@author: Sean_Woodward
+"""
+
--- Workcell_LEDs/SLAS_Controller/SlasMain.py
+++ Workcell_LEDs/SLAS_Controller/SlasMain.py
@@ -14,6 +14,7 @@ import time
 import logging
 import board
 import neopixel
+import numpy as np
 
 #Setup logging
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] - %(asctime)s - (%(threadName)-10s) %(message)s')
@@ -28,17 +29,34 @@ class Workcell():
         self.ledCount = ledCount
         self.ledBrightness = ledBrightness
         self.ledOrder = ledOrder
+        self.ledArray = np.zeros((ledCount,4))
+        self.ledArray[:,3] = 1.0
     
     def LedInitialise(self):
         self.ledStrip = neopixel.NeoPixel(self.ledPin, self.ledCount, brightness = self.ledBrightness, pixel_order=self.ledOrder, auto_write=False)
     
-    def LedSections(self, sections = [[1,[0,0]]]):
+    def LedSections(self, sections = [[0,0]]):
         self.ledSections = sections
+    
+    def LedSectionAnimations(self, animations = ["rainbow"]): #default rainbow used
+        self.ledSectionAnimations = animations
+    
+    def AnimationCall(self, input):
+            method = getattr(self,input)
+            return method()
     
     def PrintLedSections(self):
         print (self.ledSections)
-        
-
+    
+    def UpdateBySection(self):
+        for i in len(self.ledSections):
+            subset = self.ledSections[i]
+            self.ledArray[subset[0]:subset[1]+1]
+    
+    def OutputLeds(self):
+        for i in range(len(self.ledCount)):
+            self.ledStrip[i] = np.rint(self.ledArray[i,0:3]*self.ledArray[i,3]).dtype(int)
+        self.ledStrip.show()
 
 
 
@@ -55,7 +73,7 @@ if __name__ == '__main__':
     SLAS.LedInitialise()
     
     logging.debug("Setting up LED sections")
-    SLAS.LedSections([[1,[0,98]]])
+    SLAS.LedSections([[0,50],[51,98]])
     
     logging.debug("Printing LED sections")
     SLAS.PrintLedSections()
