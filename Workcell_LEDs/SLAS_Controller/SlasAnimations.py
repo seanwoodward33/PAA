952a91f1138310ce34c78893a399fa00e517cdd9
Sean Woodward <seanwoodward33@gmail.com>
2019-12-09 01:20:19 +0000

    Adding functions to SlasMain

--- Workcell_LEDs/SLAS_Controller/SlasAnimations.py
+++ Workcell_LEDs/SLAS_Controller/SlasAnimations.py
@@ -5,3 +5,24 @@ Created on Fri Dec  6 10:06:26 2019
 @author: Sean_Woodward
 """
 
+import colorsys
+
+"""Functions to control LEDs"""
+#Non-normalised HSV to RGB function
+def HsvToRgb(h,s,v):
+    return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
+
+#Run complete - Run rainbow animation
+def RunComplete(self, section):
+    self.ledArray[section[0],section[1]+1][:,3] = 1
+    ledCount = len(self.ledArray[section[0],section[1]+1])
+    if self.firstRun == True:
+        while self.animationRun == True:
+            for i in range(ledCount):
+                self.ledArray[section[0] + i][0:3] = HsvToRgb((((i)%ledCount)/ledCount),1.0,1.0)
+    
+    if self.firstRun == False:
+        while self.animationRun == True:
+            for i in range(ledCount):
+                if self.animationRun == False: break
+                self.ledArray[section[0] + i][0:3] = read RGB, turn to HSV, add one, turn back to RGB
--- Workcell_LEDs/SLAS_Controller/SlasMain.py
+++ Workcell_LEDs/SLAS_Controller/SlasMain.py
@@ -16,6 +16,9 @@ import board
 import neopixel
 import numpy as np
 
+#Import other code
+import SlasAnimations
+
 #Setup logging
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] - %(asctime)s - (%(threadName)-10s) %(message)s')
 
@@ -30,33 +33,40 @@ class Workcell():
         self.ledBrightness = ledBrightness
         self.ledOrder = ledOrder
         self.ledArray = np.zeros((ledCount,4))
-        self.ledArray[:,3] = 1.0
+        self.ledArray = 1.0
+        self.animationRun = True
+        self.newRun = True
     
     def LedInitialise(self):
         self.ledStrip = neopixel.NeoPixel(self.ledPin, self.ledCount, brightness = self.ledBrightness, pixel_order=self.ledOrder, auto_write=False)
     
-    def LedSections(self, sections = [[0,0]]):
+    def LedSections(self, sections = [[0,self.ledCount]]):
         self.ledSections = sections
     
-    def LedSectionAnimations(self, animations = ["rainbow"]): #default rainbow used
+    def LedSectionAnimations(self, animations = ["RunComplete"]): #default runcomplete rainbow used
         self.ledSectionAnimations = animations
     
-    def AnimationCall(self, input):
+    def AnimationCall(self, input, section):
             method = getattr(self,input)
-            return method()
+            return method(section)
     
     def PrintLedSections(self):
         print (self.ledSections)
     
     def UpdateBySection(self):
         for i in len(self.ledSections):
-            subset = self.ledSections[i]
-            self.ledArray[subset[0]:subset[1]+1]
+            section = self.ledSections[i]
+            self.AnimationCall(self.ledSectionAnimations[i],section)
+        if self.newRun == True:
+            self.newRun = False
     
     def OutputLeds(self):
         for i in range(len(self.ledCount)):
             self.ledStrip[i] = np.rint(self.ledArray[i,0:3]*self.ledArray[i,3]).dtype(int)
-        self.ledStrip.show()
+        self.ledStrip.show()        
+    
+    def RunComplete(self, section):
+        SlasAnimations.RunComplete(self, section)
 
 
 
@@ -74,6 +84,9 @@ if __name__ == '__main__':
     
     logging.debug("Setting up LED sections")
     SLAS.LedSections([[0,50],[51,98]])
+    
+    logging.debug("Setting animation for each section")
+    SLAS.LedSectionAnimations(["RunComplete", "TeachMode"])
     
     logging.debug("Printing LED sections")
     SLAS.PrintLedSections()
