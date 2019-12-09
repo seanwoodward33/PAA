ee2a0e13cf8789922ebd4cf1fecb35065dffc083
Sean Woodward <seanwoodward33@gmail.com>
2019-12-09 23:15:16 +0000

    Testing rainbow function

--- Workcell_LEDs/SLAS_Controller/SlasMain.py
+++ Workcell_LEDs/SLAS_Controller/SlasMain.py
@@ -62,9 +62,11 @@ class Workcell():
     
     def OutputLeds(self):
         print(self.ledStrip[0])
+        print(self.ledStrip[-1])
         for i in range(self.ledCount):
             self.ledStrip[i] = (int(self.ledArray[i][0]*self.ledArray[i,3]),int(self.ledArray[i][1]*self.ledArray[i,3]),int(self.ledArray[i][2]*self.ledArray[i,3]))#.dtype(int) np.rint()
         print(self.ledStrip[0])
+        print(self.ledStrip[-1])
         self.ledStrip.show()        
     
     def RunComplete(self, section):
