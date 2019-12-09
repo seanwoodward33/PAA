b556cfbcc5fb3e442be52fe76ab4529506a33a25
Sean Woodward <seanwoodward33@gmail.com>
2019-12-09 22:59:11 +0000

    Testing rainbow function

--- Workcell_LEDs/SLAS_Controller/SlasAnimations.py
+++ Workcell_LEDs/SLAS_Controller/SlasAnimations.py
@@ -17,13 +17,13 @@ def RunComplete(self, section):
     self.ledArray[section[0]:section[1]+1][:,3] = 1
     ledCount = len(self.ledArray[section[0]:section[1]+1])+1
     if self.firstRun == True:
-        while self.animationRun == True:
-            for i in range(ledCount):
-                for j in range(3):
-                    self.ledArray[section[0] + i][j] = HsvToRgb((((i)%ledCount)/ledCount),1.0,1.0)[j]
+        #while self.animationRun == True:
+        for i in range(ledCount):
+            for j in range(3):
+                self.ledArray[section[0] + i][j] = HsvToRgb((((i)%ledCount)/ledCount),1.0,1.0)[j]
     
     if self.firstRun == False:
-        while self.animationRun == True:
-            for i in range(ledCount):
-                if self.animationRun == False: break
-                self.ledArray[section[0] + i][0:3] = self.ledArray[section[0] + i - 1][0:3]
+        #while self.animationRun == True:
+        for i in range(ledCount):
+            if self.animationRun == False: break
+            self.ledArray[section[0] + i][0:3] = self.ledArray[section[0] + i - 1][0:3]
