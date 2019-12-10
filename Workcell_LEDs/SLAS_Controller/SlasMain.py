acdcc6543ffb1ff787bb416f5cccf57d19842f8e
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-10 17:33:01 +0000

    Added door open animation

--- Workcell_LEDs/SLAS_Controller/SlasMain.py
+++ Workcell_LEDs/SLAS_Controller/SlasMain.py
@@ -64,6 +64,7 @@ class Workcell():
     def OutputLeds(self):
         for i in range(self.ledCount):
             x = np.rint(self.ledArray[i][0:3]*self.ledArray[i,3]).astype(int)
+            print(x)
             self.ledStrip[i] = (x[0],x[1],x[2])
         self.ledStrip.show()        
     
