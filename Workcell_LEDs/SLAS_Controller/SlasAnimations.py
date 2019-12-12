32ea16386b83c3dfa7de1a9c18276be7a6cffef5
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-12 16:24:54 +0000

    Add SystemRunningLong animations

--- Workcell_LEDs/SLAS_Controller/SlasAnimations.py
+++ Workcell_LEDs/SLAS_Controller/SlasAnimations.py
@@ -96,7 +96,7 @@ def SystemRunningLong(self, i):
     
     if self.firstRun[i]  == False:
         self.percentageComplete = (self.finishTime - datetime.datetime.now())/self.runLength
-        print (self.finishTime + " " + self.datetime.datetime.now() + " " + self.percentageComplete)
+        print (str(self.finishTime) + " " + str(datetime.datetime.now()) + " " + str(self.percentageComplete))
         if self.percentageComplete > 1:
             self.percentageComplete = 1
             self.LedSectionAnimations[i] = "RunComplete"
