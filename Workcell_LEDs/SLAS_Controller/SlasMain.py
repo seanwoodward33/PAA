b191d7c12726c1d21bbde8513f23c1462cd9dc69
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-12 16:19:16 +0000

    Add SystemRunningLong animations

--- Workcell_LEDs/SLAS_Controller/SlasMain.py
+++ Workcell_LEDs/SLAS_Controller/SlasMain.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     #SLAS.LedSectionAnimations(["RunComplete", "TeachMode"])
     
     #Create list of all programmed animations to cycle through
-    animationsTaught = ["RunComplete", "TeachMode", "DoorOpen", "SystemRunningShort", "SystemRunningLong"]
+    animationsTaught = ["SystemRunningShort", "SystemRunningLong"] #["RunComplete", "TeachMode", "DoorOpen", "SystemRunningShort", "SystemRunningLong"]
     logging.debug("Setting animation to be first two animations in animationsTaught list")
     SLAS.LedSectionAnimations([animationsTaught[0], animationsTaught[1]])
     
