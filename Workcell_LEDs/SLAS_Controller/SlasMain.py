13cdc84bf77fc394e5c33838f6353acd29467617
Sean Woodward <Sean_Woodward@Falcon.local>
2019-12-23 14:17:37 +0000

    Add queue to threading

--- Workcell_LEDs/SLAS_Controller/SlasMain.py
+++ Workcell_LEDs/SLAS_Controller/SlasMain.py
@@ -164,7 +164,7 @@ class SafetySystem(threading.Thread):
         while True:
             self.doors[0] = self.pin.value
             if self.doors[0] != self.lastDoors[0]:
-                logging.debug("Change in self.doors")
+                logging.debug("Change in inputs")
                 while runQ.empty() == False:
                     runQ.get()
                 runQ.put(self.doors)
